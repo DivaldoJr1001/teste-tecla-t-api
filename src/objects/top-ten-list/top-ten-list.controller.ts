164d69c18d64214bf19b1d3b6d80352b59aa8a48
Divaldo Aderaldo de Oliveira Júnior <divaldojr9@gmail.com>
2023-07-12 20:02:47 -0300

    Testes Unitários - Top 10 CRUD

--- src/objects/top-ten-list/top-ten-list.controller.ts
+++ src/objects/top-ten-list/top-ten-list.controller.ts
@@ -3,7 +3,7 @@ import { TopTenListService } from './top-ten-list.service';
 import { TopTenList } from './top-ten-list.schema';
 import { AuthGuard } from './../../auth/auth.guard';
 
-@Controller('top-ten-list')
+@Controller('toptenlist')
 export class TopTenListController {
 
   constructor(private topTenListService: TopTenListService) { }
@@ -11,21 +11,5 @@ export class TopTenListController {
   @Get()
   async get() {
     return await this.topTenListService.get();
-  }
-
-  @UseGuards(AuthGuard)
-  @Post('/create')
-  async createTopTenList(@Body() topTenList: TopTenList) {
-    topTenList._id = '0';
-    const newTopTenList = await this.topTenListService.create(topTenList);
-    return newTopTenList;
-  }
-
-  @UseGuards(AuthGuard)
-  @Put('/update/:id')
-  async updateTopTenList(
-    @Body() topTenList: TopTenList,
-  ) {
-    return await this.topTenListService.update(topTenList);
   }
 }
